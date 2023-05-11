e948466777613353412c0192784b8a28ec17bc48
Pawan Rama Mali <prm@outlook.in>
2023-05-11 21:19:17 +0530

    Create content.R

---
+++ app/view/content.R
@@ -0,0 +1,47 @@
+box::use(
+  shiny[...],
+  bslib[...],
+)
+
+box::use(
+  app/view/card[...],
+)
+
+ui <- function(id) {
+  ns <- NS(id)
+  fluidPage(
+    theme = bs_theme(version=5),
+
+    fluidRow(
+      column(3,
+             bs_card(),
+             bs_card()
+      ),
+      column(3,
+
+             
+             bs_card(),
+             bs_card()
+      ),
+      column(3,
+
+             
+             bs_card(),
+             bs_card()
+      ),
+      column(3,
+
+             
+             bs_card(),
+             bs_card(),
+             bs_card(),
+             bs_card()
+      )
+      
+    )
+  )
+
+
+}
+
+server <- function(id) {}
